88d26adab08b6ac1a4410e47040a42ef72bc6fc8
Suyesh Bhandari <suyeshb@gmail.com>
2015-07-15 17:21:45 -0400

    First Commit

---
+++ crowdfund.rb
@@ -0,0 +1,10 @@
+project1 = "ABC"
+fund1 = 1000
+project2 = "LMN"
+fund2 = 2000
+project3 = "XYZ"
+fund3 = 1500
+
+puts "Project #{project1} has #{fund1} in funding"
+
+puts "Projects:\n\t#{project1}\n\t#{project2}\n\t#{project3}"
